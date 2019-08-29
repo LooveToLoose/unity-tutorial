1249deab720d6e9273834ff01a3b79b1817b6208
Jonas Tyroller <tyroller.jonas@gmail.com>
2019-08-29 15:44:24 +0200

    Tutorial 3 Update

---
+++ First Game Experiments/Assets/Scripts/General/AddConstantVelocity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddConstantVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Force;
+	
+	void FixedUpdate ()
+    {
+        GetComponent<Rigidbody>().velocity += v3Force;
+	}
+}
---
+++ First Game Experiments/Assets/Scripts/Specific/Bridge.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            animator.SetTrigger("Lower Bridge");
+        }
+    }
+}
