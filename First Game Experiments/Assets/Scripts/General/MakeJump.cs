23265f070ebcb166d976357cbf8257fbf52a7467
Nikolai Kulikov <nikolai.kulikov.dev@gmail.com>
2021-02-22 22:19:11 +0200

    add simple jump

---
+++ First Game Experiments/Assets/Scripts/General/MakeJump.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeJump : MonoBehaviour
+{
+    [SerializeField]
+    float jumpForce;
+
+    [SerializeField]
+    KeyCode jumpKey;
+
+    bool onTheGround = true;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onTheGround = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(jumpKey) && onTheGround) {
+            GetComponent<Rigidbody>().velocity += new Vector3(0, jumpForce, 0);
+            onTheGround = false;
+        }
+    }
+}
